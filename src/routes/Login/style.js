8fc09b1783ed8c3f068cb0f3cfd256e1152e5236
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-15 19:52:12 -0300

    feat: add style in page/Login

--- src/routes/Login/style.js
+++ src/routes/Login/style.js
@@ -8,8 +8,33 @@ const Title = styled.div`
   color: ${COLOR.WHITE};
 `;
 
-const Bla = styled.div`
+const Input = styled.input.attrs((props) => ({
+  type: 'text',
+  placeholder: props.text,
+}))`
+  width: 21em;
+  height: 3.5em;
+  border: none;
+  border-radius: 0.3em;
   color: #000;
+  padding: 0 0.5em;
+
+  &&::placeholder {
+    color: rgba(0, 0, 0, 1);
+  }
 `;
 
-export { Title, Bla };
+const Label = styled.label`
+  display: flex;
+`;
+
+const SignUp = styled.p`
+  color: ${COLOR.WHITE};
+  font-weight: 700;
+  text-align: center;
+  font-size: 0.8em;
+`;
+
+export {
+  Title, Input, Label, SignUp,
+};

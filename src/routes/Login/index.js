708b67f8a534aede51b207af217aa03c8b9ea3e8
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-15 19:51:10 -0300

    feat: create page Login

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,7 +1,33 @@
-import { Title } from './style';
+import { Link } from 'react-router-dom';
+import ModelForm from '../../components/ModelForm';
+import {
+  Title, Input, Label, SignUp,
+} from './style';
 
 export default function Login() {
   return (
-    <Title>Daniel Bernardino de Souza</Title>
+    <>
+      <Title>MyWallet</Title>
+      <ModelForm signUp={false}>
+        <Label label="id-email">
+          <Input
+            text="E-mail"
+            id="id-email"
+          />
+        </Label>
+        <Label label="id-password">
+          <Input
+            text="Senha"
+            id="id-password"
+          />
+        </Label>
+      </ModelForm>
+      <Link
+        style={{ textDecoration: 'none' }}
+        to="/sign-up"
+      >
+        <SignUp>Primeira vez? Cadastre-se!</SignUp>
+      </Link>
+    </>
   );
 }

9fc205b44265062ac4044ea45d6dae713f711380
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-17 09:59:12 -0300

    refactor: change forms in pages login/register

--- src/components/ModelForm/index.js
+++ src/components/ModelForm/index.js
@@ -1,22 +1,76 @@
 import PropTypes from 'prop-types';
-import { Forms, ButtonSubmit } from './style';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  Forms, ButtonSubmit, Label, Input,
+} from './style';
+import { AuthContext } from '../../contexts/authContext';
+import { registerUser } from '../../helpers/authHelpers';
 
-export default function ModelForm({ children, signUp }) {
-  function bla() {
-    return true;
+export default function ModelForm({ signUp, login }) {
+  const { auth, setAuth } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  let typeForm;
+  if (login) typeForm = false;
+  if (signUp) typeForm = true;
+
+  async function toRegister(e) {
+    e.preventDefault();
+    registerUser(auth).then((token) => {
+      setAuth({
+        token, name: '', email: '', password: '', repeatPassword: '',
+      });
+    });
+    navigate('/');
   }
   function ble() {
     return false;
   }
   return (
-    <Forms onSubmit={() => (signUp ? bla() : ble())}>
-      {children}
-      <ButtonSubmit value={signUp ? 'Cadastrar' : 'Entrar'} />
+    <Forms onSubmit={(e) => (typeForm ? toRegister(e) : ble(e))}>
+      {typeForm && (
+        <Label label="id-name">
+          <Input
+            text="Nome"
+            id="id-name"
+            onChange={(e) => setAuth({ ...auth, name: e.target.value })}
+          />
+        </Label>
+      )}
+
+      <Label label="id-email">
+        <Input
+          text="E-mail"
+          id="id-email"
+          onChange={(e) => setAuth({ ...auth, email: e.target.value })}
+        />
+      </Label>
+
+      <Label label="id-password">
+        <Input
+          text="Senha"
+          id="id-password"
+          onChange={(e) => setAuth({ ...auth, password: e.target.value })}
+        />
+      </Label>
+
+      {typeForm && (
+        <Label label="id-repeat-password">
+          <Input
+            text="Confirme a senha"
+            id="id-repeat-password"
+            onChange={(e) => setAuth(
+              { ...auth, repeatPassword: e.target.value },
+            )}
+          />
+        </Label>
+      )}
+      <ButtonSubmit value={typeForm ? 'Cadastrar' : 'Entrar'} />
     </Forms>
   );
 }
-
 ModelForm.propTypes = {
-  children: PropTypes.any,
   signUp: PropTypes.bool,
+  login: PropTypes.bool,
 };
--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,27 +1,14 @@
 import { Link } from 'react-router-dom';
 import ModelForm from '../../components/ModelForm';
-import {
-  Title, Input, Label, SignUp,
-} from '../../components/ModelForm/style';
+import { Title, SignUp } from '../../components/ModelForm/style';
 
 export default function Login() {
   return (
     <>
       <Title>MyWallet</Title>
-      <ModelForm signUp={false}>
+      <ModelForm login />
 
-        <Label label="id-email">
-          <Input text="E-mail" id="id-email" />
-        </Label>
-        <Label label="id-password">
-          <Input text="Senha" id="id-password" />
-        </Label>
-
-      </ModelForm>
-      <Link
-        style={{ textDecoration: 'none' }}
-        to="/sign-up"
-      >
+      <Link style={{ textDecoration: 'none' }} to="/sign-up">
         <SignUp>Primeira vez? Cadastre-se!</SignUp>
       </Link>
     </>

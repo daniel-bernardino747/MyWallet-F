f018b32c211217de3e4d887cea584f199168e0c3
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-18 23:32:50 -0300

    refactor: remove useless function

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import IconExit from '../../assets/icon-exit.svg';
 import Screen from '../../components/Screen';
 import Header from '../../components/Header';
@@ -9,14 +9,6 @@ import { Container, Exit } from './style';
 
 export default function Home() {
   const [user, setUser] = useState(window.localStorage.getItem('user'));
-  const navigate = useNavigate();
-
-  function exitSession() {
-    window.localStorage.removeItem('token');
-    window.localStorage.removeItem('user');
-    navigate('/');
-  }
-
   return (
     <Container>
       <Header>
@@ -25,7 +17,9 @@ export default function Home() {
           {' '}
           {user}
         </p>
-        <Exit onClick={() => exitSession()} src={IconExit} alt="Icon to exit the App" />
+        <Link to="/login">
+          <Exit src={IconExit} alt="Icon to exit the App" />
+        </Link>
       </Header>
 
       <Screen />

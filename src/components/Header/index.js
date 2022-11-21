aa8153dae9c129664927c77a8a34d648414e4e64
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-11-21 10:16:26 -0300

    refactor: change let to const variable

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,21 +3,18 @@ import { Container, Welcome } from './style';
 
 export default function Header({ children }) {
   const justTitle = typeof children === 'string';
-  let h1Page;
-  let headerContent;
+  const content = (!justTitle)
+    ? { h1: children[0], header: children.slice(1) }
+    : { h1: '', header: '' };
 
-  if (!justTitle) {
-    h1Page = children[0];
-    headerContent = children.slice(1);
-  }
   return (
     <Container>
       {justTitle ? (
         <Welcome>{children}</Welcome>
       ) : (
         <>
-          <Welcome>{h1Page}</Welcome>
-          {headerContent}
+          <Welcome>{content.h1}</Welcome>
+          {content.header}
         </>
       )}
     </Container>
